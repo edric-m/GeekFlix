0236b6f1fbe230c85f625189df504d936f408f83
edric <edjomendoza@gmail.com>
2020-04-02 19:42:01 +1100

    filter working but not ideal functionality

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -18,7 +18,7 @@ export class UserlistComponent implements OnInit {
   constructor(private getListService: GetlistService) { }
 
   ngOnInit(): void {
-    this.list = [324857,1726];
+    this.list = [];//[324857,1726];
     this.userName = '_USER_';
     this.listOfOption = [
       '😀',
@@ -39,33 +39,60 @@ export class UserlistComponent implements OnInit {
   }
 
   emotionAdded() {
-    let emotions = [];
+    //let emotions = [];
+    let filteredList: number[] = [];
     
-    for(let emotion of this.listOfSelectedValue) {
-      emotions.push(emotion);
+    for(let emotion of this.listOfSelectedValue) { //TODO: cant get this to work but is ideally the functionality wanted
+      //emotions.push(emotion);
+      let emoId = this.listOfOption.indexOf(emotion);
+      //let emoId = 8;
+      let emoList = localStorage.getItem('emoji'+emoId);
+      if(emoList === null) {
+        //do nothing
+      } else {
+        let x = emoList; //localStorage.getItem('emoji'+emoId);
+        let x2: number[] = JSON.parse(x);
+        //this.list = [...this.list,...x2]; //concat two arrays
 
-      this.hasData = true;
+        //filteredList = x2; //closest answer so far
+        for(let id of x2) {
+          if(!filteredList.includes(id)) {  //TODO: 2-4-20 almost but i want to only show movies that share all emojis
+            filteredList.push(id);
+          }
+        }
+        
+        //x2.push(this.movieId);
+        //console.log(x2);
+        //localStorage.setItem('emoji'+emoId, JSON.stringify(x2));
+      }
     }
+
+    //this.list;
+    this.list = [...filteredList];
     //TODO: if listOfSelectedValue is empty delete this movie from local storage
     //localStorage.setItem(this.route.snapshot.params['id'], JSON.stringify(emotions));
 
     //fill list
-    this.findMoviesFromList(this.list);
+    this.findMoviesFromList(this.list); //does this not work because its async?
   }
 
   findMoviesFromList(list: number[]) {
+    this.movieList = [];
+    this.hasData = false;
     for(let id of list) {
+      this.hasData = true;
       this.getMovie(id);
     }
   }
 
   getMovie(movieId: number) {
     this.getListService.getMoviePage(movieId).subscribe((movie) => {
+      //modify description to be emojis
       this.movieList.push(movie);
     });
   }
 
-  //TODO: this function is duplicate code
+  //TODO: this function is duplicate code, twice now
   deleteMovie(id: number) {
     if(localStorage.getItem('removed') === null) {
       localStorage.setItem('removed', JSON.stringify([id]));
